52f58689b1ce34c92a30e728ed05feddb71be655
Vishal Patidar <vishalpatidar0102@email.com>
2025-07-12 19:30:47 +0530

    BlogCard,BlogList,Header Update and Create

--- QuickBlog/src/components/Header.jsx
+++ QuickBlog/src/components/Header.jsx
@@ -11,6 +11,16 @@ const Header = () => {
             </div>
             <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-16
              text-gray-700'>Your own <span className='text-primaary'>blogging</span> <br/>platform.</h1>
+
+             <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'>This is your space to think out loud, to share what matters, and to
+               write without filters. Whether it's one word or a thousand, your story 
+               starts right here.</p>
+
+               <form  className='flex justify-between max-w-lg max-sm:scale-75 mx-auto
+               border border-gray-300 bg-white rounded overflow-hidden'>
+                <input type="text" placeholder='Search for blogs' required className='w-full pl-4 otline-none' />
+                <button type='submit' className='bg-primaary text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer'>Search</button>
+               </form>
              
         </div>
         <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50' />
